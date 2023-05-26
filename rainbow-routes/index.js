11d82d7fca098fcce96c97658f7029ddd60fc4a5
Aaron garcia <aarongarcia@Aarons-MacBook-Pro.local>
2023-05-26 11:57:19 -0700

    layed the foundation for this project

---
+++ rainbow-routes/index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+// Declare routes that people can visit on the application.
+// Color Page
+app.get('/:color', function (req, res) {
+    let myColor = req.params.color
+    res.send(`
+        <body style="margin: 0;">
+            <div style="border: 1px solid black; height: 10vh; background-color: ${myColor};">
+                <h2 style="text-align: center;">NAV BAR</h2>
+            </div>
+            <h1 style="color: ${myColor};">${myColor} Page</h1>
+        </body>
+    `)
+})
+
+
+app.listen(process.env.PORT)
